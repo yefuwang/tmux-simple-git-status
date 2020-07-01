f856cf7b8e09085ec3c479016e35fc1639346e45
Yefu Wang <yefu.wang@kanopy.com>
2020-07-01 08:48:10 -0700

    Add the remote branch and untracked files.
    Signed-off-by: Yefu Wang <yefu.wang@kanopy.com>

--- scripts/git_status.sh
+++ scripts/git_status.sh
@@ -3,6 +3,11 @@
 PANE_PATH=$(tmux display-message -p -F "#{pane_current_path}")
 cd $PANE_PATH
 
+# Returns the number of untracked files
+function evil_git_num_untracked_files {
+   expr `git status --porcelain 2>/dev/null| grep "^??" | wc -l`
+}
+
 git_changes() {
   local changes=$(git diff --shortstat | sed 's/^[^0-9]*\([0-9]*\)[^0-9]*\([0-9]*\)[^0-9]*\([0-9]*\)[^0-9]*/\1;\2;\3/')
   local changes_array=(${changes//;/ })
@@ -20,6 +25,12 @@ git_changes() {
     result+=("-${changes_array[2]}")
   fi
 
+  local untracked=$(evil_git_num_untracked_files)
+
+  if [ "${untracked}" -ne "0" ]; then
+      result+=("..u${untracked}")
+  fi
+
   local joined=$(printf " %s" "${result[@]}")
   local joined=${joined:1}
 
@@ -30,10 +41,11 @@ git_changes() {
 
 git_status() {
   local status=$(git rev-parse --abbrev-ref HEAD)
+  local remote=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
   local changes=$(git_changes)
 
   if [[ -n $status ]]; then
-    printf " $status $changes"
+      printf "▓${status}▓${remote} $changes"
   fi
 }
 
